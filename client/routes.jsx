e222789b8c824bd898fc60dcd14ee1b564648347
Matthew Henkler <matt@henkler.org>
2016-02-26 08:51:25 -0600

    first commit

---
+++ client/routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'react-mounter';
+
+import {Layout, Welcome} from './app.jsx';
+
+FlowRouter.route("/", {
+  action() {
+    mount(Layout, {
+      content: (<Welcome name="arunoda" />)
+    });
+  }
+});
